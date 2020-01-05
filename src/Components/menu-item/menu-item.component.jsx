6bee81cc85243698f0a511f8f08b29156ec3f223
Sunny12jun <Sunny12jun@gmail.com>
2020-01-05 12:26:30 +0530

    update our menu item to nevigate with router

--- src/Components/menu-item/menu-item.component.jsx
+++ src/Components/menu-item/menu-item.component.jsx
@@ -1,34 +1,27 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom'
 
 import './menu-item.styles.scss';
 
-// const MenuItem = (({ image, size, title }) => {
-//     return <div className={`${size} menu-item`}  >
-//           <div
-//       className='background-image'
-//       style={{
-//         backgroundImage: `url(${image})`
-//       }}
-//     />
-//         <div className='content'>
-//             <div className='title'>{title}</div>
-//             <span className='subtitle'>SHOP NOW</span>
-//         </div>
-     
-//     </div>
-// })
-const MenuItem = ({image, size, title}) => (
-    <div className={`${size} menu-item`}>
-      <div
-        className='background-image'
-        style={{
-          backgroundImage: `url(${image})`
-        }}
-      />
-      <div className='content'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
-        <span className='subtitle'>SHOP NOW</span>
-      </div>
+
+
+
+const MenuItem = ({ imageUrl, size, title, history, linkUrl, match }) => (
+
+  <div className={`${size} menu-item`} onClick={() => { history.push(`${match.url}${linkUrl}`); console.log(match); console.log(history);
+   console.log(linkUrl)
+   }}>
+    
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+    <span className='subtitle'>SHOP NOW </span>
     </div>
-  );
-export default MenuItem;
+  </div>
+);
+export default withRouter(MenuItem);
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import Directory from '../../components/directory/directory.component';=
+   import Directory from '../../Components/directory/directory.componet';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
+
+export default HomePage;
